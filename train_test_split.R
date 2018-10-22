e35252d9e2542fee36b3447cc143541bb7b003f1
jeroldyu <jeroldyu@stanford.edu>
2018-10-22 16:01:52 -0700

    Added train and test CSV's

--- train_test_split.R
+++ train_test_split.R
@@ -1,4 +1,4 @@
-# setwd("/Users/jeroldyu/Desktop/ms&e226/project/nba-stats")
+# setwd("/Users/jeroldyu/Documents/GitHub/mse226-mini-project")
 
 library(tidyverse)
 library(readxl)
@@ -6,7 +6,14 @@ library(readxl)
 set.seed(1)
 
 advanced_stats <- read_xlsx("NBA Players - Advanced Season Stats (1978-2016).xlsx",
-                             sheet = "Hoja1")
+                             sheet = "Hoja1") %>%
+  filter(year < 2016) %>%
+  select(-c(column_s, column_x))
+
 test_idx <- sample(1:nrow(advanced_stats), .2 * nrow(advanced_stats))
 
 train <- advanced_stats[-test_idx,]
+test <- advanced_stats[test_idx,]
+
+write.csv(train, "train.csv", row.names = F)
+write.csv(test, "test.csv", row.names = F)
