121100c5b83a52c521478fa9b1fdeb0bf98b21c6
ajstroud <rednaxelastroud@gmail.com>
2018-10-23 22:32:47 -0700

    adding columns to data

--- train_test_split.R
+++ train_test_split.R
@@ -8,7 +8,9 @@ set.seed(1)
 advanced_stats <- read_xlsx("NBA Players - Advanced Season Stats (1978-2016).xlsx",
                              sheet = "Hoja1") %>%
   filter(year < 2016) %>%
-  select(-c(column_s, column_x))
+  select(-c(column_s, column_x)) %>%
+  mutate(missing_salary = is.na(truesalary),
+         overpaid = truesalary > production)
 
 test_idx <- sample(1:nrow(advanced_stats), .2 * nrow(advanced_stats))
 
