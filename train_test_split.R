4044f930886e94ef3fe886cccdcb090b976a1fcd
ajstroud <rednaxelastroud@gmail.com>
2018-10-24 15:23:57 -0700

    fixing salary transformation and overpaid calculation

--- train_test_split.R
+++ train_test_split.R
@@ -9,6 +9,8 @@ set.seed(1)
 # numeric value (e.g. 1000.25).
 dollar_to_numeric <- function(dollar) {
   dollar <- gsub(",", "", dollar)
+  dollar <- gsub("(", "", dollar, fixed=TRUE)
+  dollar <- gsub(")", "", dollar, fixed=TRUE)
   dollar %>%
     as.character() %>%
     substring(2) %>%
@@ -19,9 +21,11 @@ advanced_stats <- read_xlsx("NBA Players - Advanced Season Stats (1978-2016).xls
                              sheet = "Hoja1") %>%
   filter(year < 2016) %>%
   select(-c(column_s, column_x)) %>%
-  mutate(missing_salary = is.na(truesalary),
-         overpaid = truesalary > production,
-         truesalary = dollar_to_numeric(truesalary))
+  mutate(truesalary = dollar_to_numeric(truesalary),
+         production = dollar_to_numeric(production),
+         missing_salary = is.na(truesalary),
+         overpaid = truesalary > production
+         )
 
 test_idx <- sample(1:nrow(advanced_stats), .2 * nrow(advanced_stats))
 
